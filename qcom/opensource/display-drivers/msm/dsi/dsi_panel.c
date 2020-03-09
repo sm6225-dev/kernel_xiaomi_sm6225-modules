616e4a9479177261471d8bd706af7f7bcc7125a2
Nauval Rizky <enuma.alrizky@gmail.com>
2020-03-09 11:54:52 +0900

    display-drivers: msm: Adapt exposure adjustment methods for spes
    - This commit was originally made for davinci but it helps for us too as it retains backlight level prior to enabling DC Dim.
    
    Change-Id: I00691fa75fde44c9e380e9f62c5fc7911c04b305
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    [muralivijay: Adapt to spes ]
    Signed-off-by: muralivijay <muralidharan9845@gmail.com>
    Signed-off-by: Guidix <guidixm@gmail.com>

--- qcom/opensource/display-drivers/msm/dsi/dsi_panel.c
+++ qcom/opensource/display-drivers/msm/dsi/dsi_panel.c
@@ -18,6 +18,10 @@
 #include "sde_dsc_helper.h"
 #include "sde_vdc_helper.h"
 
+#ifdef CONFIG_TARGET_PROJECT_K7T
+#include "exposure_adjustment.h"
+#endif
+
 /**
  * topology is currently defined by a set of following 3 values:
  * 1. num of layer mixers
@@ -696,10 +700,18 @@ int dsi_panel_set_doze_mode(struct dsi_panel *panel, enum dsi_doze_mode_type mod
 int dsi_panel_set_backlight(struct dsi_panel *panel, u32 bl_lvl)
 {
 	int rc = 0;
+#ifdef CONFIG_TARGET_PROJECT_K7T
+	int bl_dc_min = panel->bl_config.bl_min_level * 2;
+#endif
 	struct dsi_backlight_config *bl = &panel->bl_config;
 
 	if (panel->host_config.ext_bridge_mode)
 		return 0;
+
+#ifdef CONFIG_TARGET_PROJECT_K7T
+        if (bl_lvl > 0)
+                bl_lvl = ea_panel_calc_backlight(bl_lvl < bl_dc_min ? bl_dc_min : bl_lvl);
+#endif
 
 	DSI_DEBUG("backlight type:%d lvl:%d\n", bl->type, bl_lvl);
 	switch (bl->type) {
