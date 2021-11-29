b7de7bffe81898a4cc2a4d90f78e666d483b9b15
maniac103 <dannybaumann@web.de>
2021-11-29 16:44:24 +0200

    display-drivers: msm: dsi: Add support for high brightness mode (HBM)
    - Our device supports HBM yet we don't have HBM support in kernel so let's add it.
    
    [cyberknight777: adapt to mojito]
    Change-Id: I71bcd8e8e242cbb75003ab825db6daf5001cd95b
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: Guidix <guidixm@gmail.com>

--- qcom/opensource/display-drivers/msm/dsi/dsi_panel.h
+++ qcom/opensource/display-drivers/msm/dsi/dsi_panel.h
@@ -279,6 +279,9 @@ struct dsi_panel {
 	enum dsi_panel_physical_type panel_type;
 
 	struct dsi_panel_ops panel_ops;
+
+    int hbm_mode;
+
 };
 
 static inline bool dsi_panel_ulps_feature_enabled(struct dsi_panel *panel)
@@ -415,4 +418,8 @@ int dsi_panel_create_cmd_packets(const char *data, u32 length, u32 count,
 void dsi_panel_destroy_cmd_packets(struct dsi_panel_cmd_set *set);
 
 void dsi_panel_dealloc_cmd_packets(struct dsi_panel_cmd_set *set);
+
+
+int dsi_panel_apply_hbm_mode(struct dsi_panel *panel);
+
 #endif /* _DSI_PANEL_H_ */
