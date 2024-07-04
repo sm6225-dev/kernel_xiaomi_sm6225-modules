03ffcf5ab2d1f06221ef4880a9e2a5a7d51e3ce6
Aman Kumar <amakum@qti.qualcomm.com>
2024-07-04 15:17:39 +0530

    cnss_prealloc: Fix a race condition to avoid spinlock access before init
    Currently, spinlock pool_table_lock is initialized only after all
    the mem pools are allocated by supported platform driver during probe.
    
    There can be a scenario when unsupported host driver load is in progress
    and it accesses the spinlock for malloc and at same time supported
    platform driver is in process of initialization of mempools. This
    leads to access of pool_table_lock spinlock before it is initialized.
    
    To fix this issue add a flag to check if mempool initialization is
    done or not before accessing the spinlock.
    
    Change-Id: Ice8f50ad60457864d2610ed590712f31096f3ede
    CRs-Fixed: 3861994

--- qcom/opensource/wlan/platform/cnss_prealloc/cnss_prealloc.c
+++ qcom/opensource/wlan/platform/cnss_prealloc/cnss_prealloc.c
@@ -110,6 +110,7 @@ static struct cnss_pool cnss_pools_wcn6750[] = {
 struct cnss_pool *cnss_pools;
 unsigned int cnss_prealloc_pool_size = ARRAY_SIZE(cnss_pools_default);
 spinlock_t pool_table_lock;
+bool mempool_initialization_done;
 
 /**
  * cnss_pool_alloc_threshold() - Allocation threshold
@@ -211,6 +212,7 @@ static int cnss_pool_init(void)
 		cnss_stack_track_init(&cnss_pools[i]);
 	}
 
+	mempool_initialization_done = true;
 	spin_lock_init(&pool_table_lock);
 
 	return 0;
@@ -242,6 +244,7 @@ static void cnss_pool_deinit(void)
 
 		cnss_stack_track_deinit(&cnss_pools[i]);
 	}
+	mempool_initialization_done = false;
 }
 
 static void cnss_assign_prealloc_pool(unsigned long device_id)
@@ -534,7 +537,7 @@ void *wcnss_prealloc_get(size_t size)
 	int i;
 	int ret = 0;
 
-	if (!cnss_pools)
+	if (!cnss_pools || !mempool_initialization_done)
 		return mem;
 
 	if (in_interrupt() || !preemptible() || rcu_preempt_depth())
@@ -595,7 +598,7 @@ int wcnss_prealloc_put(void *mem)
 	int ret;
 	unsigned long irq_flags;
 
-	if (!mem || !cnss_pools)
+	if (!mem || !cnss_pools || !mempool_initialization_done)
 		return 0;
 
 	for (i = 0; i < cnss_prealloc_pool_size; i++) {
@@ -665,7 +668,7 @@ int wcnss_prealloc_put(void *mem)
 	int ret;
 	unsigned long irq_flags;
 
-	if (!mem || !cnss_pools)
+	if (!mem || !cnss_pools || !mempool_initialization_done)
 		return 0;
 
 	i = cnss_pool_get_index(mem);
