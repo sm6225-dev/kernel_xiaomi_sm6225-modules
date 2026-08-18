7704a9ff438d562c4adc76c24a400f5c12e48b49
halt-spesn <ffoxy2870@gmail.com>
2026-08-18 14:07:57 +0300

    remove qcom/opensource/video-driver due to compatibility issues

--- qcom/opensource/video-driver/config/neo_video.h
+++
@@ -1,7 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0-only */
-/*
- * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
- */
-
-#define CONFIG_MSM_VIDC_NEO      1
-#define CONFIG_MSM_VIDC_IRIS3    1
--- qcom/opensource/video-driver/config/ravelin_video.h
+++
@@ -1,7 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0-only */
-/*
- * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
- */
-
-#define CONFIG_MSM_VIDC_RAVELIN 1
-#define CONFIG_MSM_VIDC_AR50LT  1
